f8cb4d54200f37da40f351bc6a29bd08c7d6542a
Andres Manco <andresmanco@gmail.com>
2018-06-14 09:54:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -18,10 +18,17 @@ function init() {
 
   body.addEventListener('keydown', function(e){
     console.log("apretaste una tecla")
+<<<<<<< HEAD
     var k = e.key;
     
+=======
+    var k = e.k;
+    console.log(k)
+    console.log(codes[index])
+>>>>>>> d238728ed6704090beafa6a5588a925b2d376cf4
     if(k === codes[index]){
       index++;
+      console.log('primer IF')
       if(index === codes.length){
         console.log("Congratulations, WUJUUU!!!")
         index = 0;
